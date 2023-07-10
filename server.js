7ea4e1a50f440bb2fb3e30a6c319f69a5d0f7210
Freddy Machaca <freddyymachaca@gmail.com>
2023-07-10 17:51:02 -0400

    crear rutas get

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import app from "./src/app.js";
 
-const port = 3000;
+const PORT = 3000;
 
 //Escutar a porta 3000
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,10 +1,38 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
+//MOCK significa simular uma API
+const selecoes = [
+  {
+    id: 1,
+    selecao: "Brasil",
+    grupo: "G",
+  },
+  {
+    id: 2,
+    selecao: "Suíça",
+    grupo: "G",
+  },
+  {
+    id: 3,
+    selecao: "Sérvia",
+    grupo: "G",
+  },
+  {
+    id: 4,
+    selecao: "Costa Rica",
+    grupo: "G",
+  },
+];
+
 //criar uma rota padrão
-app.get('/', (req, res) => {
-    res.send('Olá Mundo!');
+app.get("/", (req, res) => {
+  res.send("API REST - Copa do Mundo");
+});
+
+app.get("/selecoes", (req, res) => {
+    res.status(200).send(selecoes);
 });
 
 export default app;
