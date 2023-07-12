b4fba66d5c6ffe0b2698ccf69c841e134d129dab
Freddy Machaca <freddyymachaca@gmail.com>
2023-07-12 08:49:37 -0400

    rutas delete y get

--- src/app.js
+++ src/app.js
@@ -29,6 +29,16 @@ const selecoes = [
   },
 ];
 
+//função para buscar a seleção
+function buscarSelecao(id) {
+  return selecoes.filter(selecao=>selecao.id == id)
+};
+
+//função para buscar o index da seleção
+function buscaIndexSelecao(id) {
+  return selecoes.findIndex(selecao=>selecao.id == id)
+};
+
 //criar uma rota padrão
 app.get("/", (req, res) => {
   res.send("API REST - Copa do Mundo");
@@ -38,9 +48,20 @@ app.get("/selecoes", (req, res) => {
     res.status(200).send(selecoes);
 });
 
+app.get('/selecoes/:id',(req,res)=>{
+    //let index = req.params.id;
+    res.json(buscarSelecao(req.params.id));
+});
+
 app.post('/selecoes', (req, res) => {
   selecoes.push(req.body);
   res.status(201).send('seleção adicionada com sucesso');
+});
+
+app.delete('/selecoes/:id', (req, res) => { //:id é um parâmetro
+  let index = buscaIndexSelecao(req.params.id); //req.params.id é o id que vem da url
+  selecoes.splice(index, 1); //splice é o método para remover um item do array
+  res.status(200).send('seleção com id ' + req.params.id + ' removida com sucesso');
 });
 
 export default app;
