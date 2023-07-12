554166e4b64bdccdb548a7978ef8eef47787f1e6
Freddy Machaca <freddyymachaca@gmail.com>
2023-07-12 16:49:15 -0400

    conexion mysql

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import app from "./src/app.js";
+import database from "./infra/conexao.js";
 
 const PORT = 3000;
 
-//Escutar a porta 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+//Conectar ao banco de dados
+database.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log("Banco de dados conectado com sucesso");
+    //Escutar a porta 3000
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+  }
 });
