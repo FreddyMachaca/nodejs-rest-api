66732ddb42fdd7109235ea47e064744ed2efefc9
Freddy Machaca <freddyymachaca@gmail.com>
2023-07-12 16:49:38 -0400

    conexion mysql

---
+++ infra/conexao.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
+
+const conexao = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+export default conexao; // Exporta la conexión para ser utilizada en otros archivos
