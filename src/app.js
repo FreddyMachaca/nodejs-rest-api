f89875c2882ee6d39b5b60a6412e3c0885610869
Freddy Machaca <freddyymachaca@gmail.com>
2023-07-12 20:52:57 -0400

    ajustes

--- src/app.js
+++ src/app.js
@@ -1,25 +1,12 @@
 import express from "express";
-import SelecaoController from "./app/controllers/SelecaoController.js";
+import routes from "./routes.js";
 
 const app = express();
 
+//usar router
+app.use(routes);
+
 //configurar o express para receber dados em json
 app.use(express.json());
-
-//ROTAS
-//GET LISTAR TODAS AS SELEÇÕES
-app.get("/selecoes", SelecaoController.index);
-
-//GET LISTAR UMA SELEÇÃO ESPECÍFICA
-app.get('/selecoes/:id',SelecaoController.show);
-
-//POST ADICIONAR UMA SELEÇÃO
-app.post('/selecoes', SelecaoController.store);
-
-//PUT ALTERAR UMA SELEÇÃO
-app.put('/selecoes/:id', SelecaoController.update);
-
-//DELETE REMOVER UMA SELEÇÃO
-app.delete('/selecoes/:id', SelecaoController.delete);
 
 export default app;
