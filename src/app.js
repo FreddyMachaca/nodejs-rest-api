a315d10d91e6b334492570ba0cac970f453932d5
Freddy Machaca <freddyymachaca@gmail.com>
2023-07-12 20:54:36 -0400

    correcion app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import routes from "./routes.js";
 
 const app = express();
 
-//usar router
-app.use(routes);
-
 //configurar o express para receber dados em json
 app.use(express.json());
+
+//usar router
+app.use(routes);
 
 export default app;
