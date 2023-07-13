f9ce80f8e3a671b2dfd0e8bd6dd70ca452a6dafe
Freddy Machaca <freddyymachaca@gmail.com>
2023-07-12 20:46:31 -0400

    refactorización repository

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -1,16 +1,33 @@
-import mysql from 'mysql';
-import dotenv from 'dotenv';
+import mysql from "mysql";
+import dotenv from "dotenv";
 
 dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
 const conexao = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 conexao.connect(); // Conecta a la base de datos
+
+/**
+ * Executa uma consulta no banco de dados com o sem valores
+ * @param {*} sql  instrução sql a ser executada
+ * @param {*} valores | [selecao, id] valores a serem pasados para a consulta
+ * @param {*} mensagemReject mensagem a ser exibida caso ocorra um erro
+ * @returns  objeto da promessa
+ */
+export const consulta = (sql, valores='',mensagemReject) => {
+  return new Promise((resolve, reject) => {
+    conexao.query(sql, valores, (erro, resultado) => {
+      if (erro) return reject(mensagemReject);
+      const row = JSON.parse(JSON.stringify(resultado));
+      return resolve(resultado);
+    });
+  });
+};
 
 export default conexao; // Exporta la conexión para ser utilizada en otros archivos
--- src/app/repositories/SelecaoRepository.js
+++ src/app/repositories/SelecaoRepository.js
@@ -1,63 +1,32 @@
-import conexao from '../database/conexao.js';
+import { consulta } from "../database/conexao.js";
 
 class SelecaoRepository {
   //CRUD
   create(selecao) {
     const sql = "INSERT INTO selecoes SET ?";
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, selecao, (erro, resultado) => {
-        if (erro) return reject("nao foi possivel cadastrar a selecao");
-        const row = JSON.parse(JSON.stringify(resultado));
-        return resolve(resultado);
-      });
-    });
+    return consulta(sql, selecao, "nao foi possivel adicionar a selecao");
   }
+
   findAll() {
     const sql = "SELECT * FROM selecoes";
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, (erro, resultado) => {
-        if (erro) return reject("nao foi possivel listar as selecoes");
-        //fazer o parse dos resultados
-        const row = JSON.parse(JSON.stringify(resultado));
-        return resolve(row);
-      });
-    });
+    return consulta(sql, "nao foi possivel listar as selecoes");
   }
 
   findById(id) {
     const sql = `SELECT * FROM selecoes WHERE id=?`;
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, id, (erro, resultado) => {
-        if (erro) return reject("nao foi possivel listar a selecao");
-        //fazer o parse dos resultados
-        const row = JSON.parse(JSON.stringify(resultado));
-        return resolve(row);
-      });
-    });
+    return consulta(sql, id, "nao foi possivel listar a selecao");
   }
 
   update(selecao, id) {
     const sql = `UPDATE selecoes SET ? WHERE id=?`;
-    return new Promise((resolve, reject) => {
-        conexao.query(sql, [selecao, id], (erro, resultado) => {
-            if (erro) return reject("nao foi possivel atualizar a selecao");
-            const row = JSON.parse(JSON.stringify(resultado));
-            return resolve(row);
-        });
-    });
+    return consulta(sql, [selecao, id], "nao foi possivel alterar a selecao");
   }
 
   delete(id) {
     const sql = `DELETE FROM selecoes WHERE id=?`;
-    return new Promise((resolve, reject) => {
-      conexao.query(sql, id, (erro, resultado) => {
-        if (erro) return reject("nao foi possivel remover a selecao");
-        const row = JSON.parse(JSON.stringify(resultado));
-        return resolve(row);
-      });
-    });
+    return consulta(sql, id, "nao foi possivel remover a selecao");
   }
-  
+
 }
 
 export default new SelecaoRepository();
